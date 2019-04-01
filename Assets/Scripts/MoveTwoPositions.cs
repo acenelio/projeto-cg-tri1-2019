629f9151138145aab1c0fa4e29aaccc28b8586d9
Nelio Alves <acenelio@gmail.com>
2019-03-31 23:06:56 -0300

    Programacao do monstro tag Monster layer Enemies

---
+++ Assets/Scripts/MoveTwoPositions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTwoPositions : MonoBehaviour
+{
+	public Transform obj = null, pos1 = null, pos2 = null;
+	public float speed = 2f;
+	public float waitTime = 0.5f;
+	Vector3 nextPos;
+
+	void Start () {
+		nextPos = pos1.localPosition;
+		StartCoroutine(Move());
+	}
+	
+	IEnumerator Move() {
+		while (true) {
+			if (obj.transform.localPosition == pos1.localPosition) {
+				nextPos = pos2.localPosition;
+				yield return new WaitForSeconds(waitTime);
+			}
+			if (obj.transform.localPosition == pos2.localPosition) {
+				nextPos = pos1.localPosition;
+				yield return new WaitForSeconds(waitTime);
+			}
+			obj.transform.localPosition = Vector3.MoveTowards(obj.transform.localPosition, nextPos, speed * Time.deltaTime);
+			yield return null;
+		}
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawLine(pos1.position, pos2.position);
+	}
+}
