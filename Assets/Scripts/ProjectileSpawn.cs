3bf0b9472cb41f093ea253bba7448807e00d37e4
Nelio Alves <acenelio@gmail.com>
2019-04-07 11:15:31 -0300

    Projectile spawn (tinha esquecido RB e DestroyWithDelay no projectile

---
+++ Assets/Scripts/ProjectileSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawn : MonoBehaviour
+{
+    public GameObject parent;
+    public GameObject prefab;
+    public float waitTime = 2f;
+    public float projectileSpeed = 8f;
+
+    public Vector3[] directions;
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn() {
+		while (true) {
+            yield return new WaitForSeconds(waitTime);
+            for (int i=0; i<directions.Length; i++) {
+                GameObject projectile = Instantiate(prefab, parent.transform.position, Quaternion.identity);
+                projectile.transform.SetParent(parent.transform);
+                projectile.GetComponent<Rigidbody2D>().velocity = directions[i] * projectileSpeed;
+            }
+		}
+	}
+}
